6135c3b6a42fc7055e00b2fdf0ca6ab17bdef0bf
Andrey Kislov <andrej.kislov@spellabs.ru>
2019-04-08 12:42:38 +0300

    Add devices to db

--- services/devices.js
+++ services/devices.js
@@ -4,6 +4,12 @@ export function addDevice(mqttClient, request) {
     if (request.topic) {
         mqttClient.subscribe(request.topic, () => logInfo(`Subscribed to topic ${request.topic}`));
 
-        // console.log(mqttClient);
+        try {
+            const res = await pool.query('INSERT INTO zigbee_devices (user_id, name, topic) VALUES($1, $2, $3)', 
+                [getUserIdByTopic(request.topic), request.name, request.topic]);
+            console.log(res.rows[0]);
+        } catch (err) {
+            console.log(err.stack);
+        }
     }
 }
