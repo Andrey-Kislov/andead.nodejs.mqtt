33e78d0a6ab3fd7db7b8e482e1c54d3b18811592
Andrey Kislov <andrej.kislov@spellabs.ru>
2019-04-15 12:55:09 +0300

    Add testing addDevice

--- services/devices.js
+++ services/devices.js
@@ -1,17 +1,41 @@
 import { logInfo } from './common';
 
-export async function addDevice(pool, mqttClient, userId, request) {
-    if (userId && request.topic) {
-        mqttClient.subscribe(request.topic, () => logInfo(`Subscribed to topic ${request.topic}`));
+export async function addDevice(pool, mqttClient, userId, request, response) {
+    if (!pool) {
+        throw new TypeError('PostgreSQL Pool must be set!');
+    }
 
+    if (!mqttClient) {
+        throw new TypeError('MQTT client must be set!');
+    }
+
+    if (userId) {
         try {
+            if (!request || !request.name || !request.topic) {
+                throw new TypeError('Invalid request object!');
+            }
+
+            mqttClient.subscribe(request.topic, () => logInfo(`Subscribed to topic ${request.topic}`));
+
             const res = await pool.query('INSERT INTO zigbee_devices (user_id, name, topic) VALUES($1, $2, $3)', 
                 [userId, request.name, request.topic]);
-            console.log(res.rows[0]);
+
+            return response.status(200).json({
+                success: new Date(),
+                message: res
+            });
         } catch (err) {
-            console.log(err.stack);
+            return response.status(500).json({
+                error: new Date(),
+                message: err.stack
+            });
         }
     }
+
+    return response.status(401).json({
+        error: new Date(),
+        message: 'Unauthorized'
+    });
 }
 
 export async function getMyDevices(pool, userId, response) {
