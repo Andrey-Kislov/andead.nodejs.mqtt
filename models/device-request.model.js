b3bfe3883cde05d77639cbc64f67e193e85060ae
ANDeaD <andead@inbox.ru>
2019-04-17 22:13:26 +0300

    Fix devices

--- models/device-request.model.js
+++ models/device-request.model.js
@@ -2,5 +2,7 @@ import joi from 'joi';
 
 export const deviceRequestSchema = joi.object().keys({
     name: joi.string().required(),
-    topic: joi.string().required()
+    topic: joi.string().required(),
+    image: joi.string().required(),
+    description: joi.string().required()
 });
